dd7c00ada2c412d96e120a0bf94ec1f8e0e73934
yamid-granda <vallenatomanizales@gmial.com>
2024-07-01 07:04:28 -0500

    feat: UI, ProductsAggregator component clean form after submit & style

--- ui/src/modules/Products/components/ProductsAggregator/index.tsx
+++ ui/src/modules/Products/components/ProductsAggregator/index.tsx
@@ -5,15 +5,19 @@ import { useState } from "react";
 import { IProductsAggregatorForm } from "./types";
 import { IOnFormChangeConfig } from "@/types";
 import { addProductFromApi } from "../../services/addProductFromApi";
+import FormGrid from "@/components/FormGrid";
+import Card from "@/components/Card";
+
+const formInitialValue = {
+  name: '',
+  price: 0,
+  quantity: 0,
+}
 
 export default function ProductsAggregator() {
   const [isAdding, setIsAdding] = useState(false);
 
-  const [form, setForm] = useState<IProductsAggregatorForm>({
-    name: '',
-    price: 0,
-    quantity: 0,
-  });
+  const [form, setForm] = useState<IProductsAggregatorForm>(formInitialValue);
 
   function onChange(config: IOnFormChangeConfig<IProductsAggregatorForm>) {
     const { key, value } = config
@@ -21,47 +25,55 @@ export default function ProductsAggregator() {
   }
 
   async function onSubmit() {
-    setIsAdding(true)
-    const addResponse = await addProductFromApi({ body: form })
-
-    if (!addResponse.isOk)
+    if (isAdding)
       return
 
-    console.log('success', addResponse);
+    setIsAdding(true)
+    await addProductFromApi({ body: form })
+    setIsAdding(false)
+    setForm(formInitialValue)
   }
 
   return (
     <Form
-      actions={<Button>Add a new Product</Button>}
       onSubmit={onSubmit}
     >
-      <Input
-        label="Name"
-        name="product-name"
-        placeholder="My new product name"
-        required
-        onChange={value => onChange({ value, key: 'name' })}
-      />
+      <Card>
+        <FormGrid>
+          <Input
+            value={form.name}
+            label="Name"
+            name="product-name"
+            placeholder="My new product name"
+            required
+            onChange={value => onChange({ value, key: 'name' })}
+          />
 
-      <Input
-        label="Price"
-        name="product-price"
-        placeholder="Selling price"
-        type="number"
-        required
-        min={0}
-        onChange={value => onChange({ value, key: 'price' })}
-      />
+          <Input
+            value={form.price}
+            label="Price"
+            name="product-price"
+            placeholder="Selling price"
+            type="number"
+            required
+            min={0}
+            onChange={value => onChange({ value, key: 'price' })}
+          />
 
-      <Input
-        label="Quantity"
-        name="product-quantity"
-        placeholder="Stock quantity"
-        type="number"
-        required
-        min={0}
-        onChange={value => onChange({ value, key: 'quantity' })}
-      />
+          <Input
+            value={form.quantity}
+            label="Quantity"
+            name="product-quantity"
+            placeholder="Stock quantity"
+            type="number"
+            required
+            min={0}
+            onChange={value => onChange({ value, key: 'quantity' })}
+          />
+        </FormGrid>
+
+        <Button>Add a new Product</Button>
+      </Card>
     </Form>
   )
 }
