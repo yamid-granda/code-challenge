2173e5f1b127c01898e3e0d9e8e4a064165e9777
yamid-granda <vallenatomanizales@gmial.com>
2024-07-01 06:12:02 -0500

    fix: UI, Navigation component app name

--- ui/src/components/Navigation/index.tsx
+++ ui/src/components/Navigation/index.tsx
@@ -33,7 +33,7 @@ export default function Navigation() {
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="self-center text-2xl whitespace-nowrap dark:text-white"><EmojiText emoji="🍺">My Cool Bar</EmojiText></span>
+          <span className="self-center text-2xl whitespace-nowrap dark:text-white"><EmojiText emoji="🍺">Cometa Bar</EmojiText></span>
         </Link>
 
         <button onClick={onOpen} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
