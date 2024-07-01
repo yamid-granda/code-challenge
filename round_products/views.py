c6a825bc4e8436c2142fc434157e19c6afcfae32
yamid-granda <vallenatomanizales@gmial.com>
2024-07-01 05:45:58 -0500

    feat: API, orders max sold stock validation

--- round_products/views.py
+++ round_products/views.py
@@ -1,11 +1,24 @@
 from rest_framework import viewsets
 from .serializer import RoundProductSerializer
 from .models import RoundProduct
+from rest_framework.exceptions import ValidationError
+from rest_framework import status
+from rest_framework.response import Response
 
 class RoundProductView(viewsets.ModelViewSet):
   serializer_class = RoundProductSerializer
   queryset = RoundProduct.objects.all()
 
   def perform_create(self, serializer):
-    product_price = serializer.validated_data['product_id'].price
+    sold_quantity = serializer.validated_data['quantity']
+    product = serializer.validated_data['product_id']
+    
+    if sold_quantity > product.quantity:
+      raise ValidationError("Quantity to sell exceeds the product stock")
+
+    product_price = product.price
     serializer.save(price=product_price)
+
+    new_quantity = product.quantity - sold_quantity
+    product.quantity = new_quantity
+    product.save()
