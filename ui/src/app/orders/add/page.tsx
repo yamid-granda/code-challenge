e531b5481f6a6134289b38eff58f4b6ea12def12
yamid-granda <vallenatomanizales@gmial.com>
2024-07-01 06:53:34 -0500

    feat: UI, add orders page update texts & add go to orders link

--- ui/src/app/orders/add/page.tsx
+++ ui/src/app/orders/add/page.tsx
@@ -1,19 +1,25 @@
 'use client'
 
+import Button from "@/components/Button";
 import PageTitle from "@/components/PageTitle";
 import Section from "@/components/Section";
-import OrderCreator from "@/modules/Orders/components/OrderCreator";
+import OrderAggregator from "@/modules/Orders/components/OrderAggregator";
+import { ORDERS_UI_PATH } from "@/modules/Orders/configs";
 
 export default function AddOrderPage() {
   return (
     <>
       <Section>
-        <PageTitle>Add new Order</PageTitle>
-        <p>Fill the following to create a new order</p>
+        <PageTitle>Add a new Order</PageTitle>
+        <p>Fill the following form to create a new order</p>
       </Section>
 
       <Section>
-        <OrderCreator />
+        <OrderAggregator />
+      </Section>
+
+      <Section>
+        <Button href={ORDERS_UI_PATH}>Go to Orders List</Button>
       </Section>
     </>
   )
