15e1ab1940602c076f5e69f95fe9a8f582d1395f
yamid-granda <vallenatomanizales@gmial.com>
2024-07-01 06:10:11 -0500

    feat: UI, Input component two way data binding & className support

--- ui/src/components/Input/index.tsx
+++ ui/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import { FormEventHandler } from "react";
+import { FormEventHandler, useEffect, useState } from "react";
 import { IInputProps } from "./types";
 
 export default function Input(props: Readonly<IInputProps>) {
@@ -6,16 +6,31 @@ export default function Input(props: Readonly<IInputProps>) {
     label,
     name,
     onChange,
+    value: initialValue,
+    className,
     ...inputProps
   } = props
 
+  const [value, setValue] = useState<string | number>(initialValue ?? '');
+
+  useEffect(() => {
+    if (initialValue !== undefined)
+      setValue(initialValue as string | number)
+  }, [initialValue])
+
   const handleChange: FormEventHandler<HTMLInputElement> = (e) => {
     const target = e.target as HTMLInputElement
-    onChange?.(target.value)
+    let value: string | number = target.value
+
+    if (inputProps.type === 'number')
+      value = Number(value)
+
+    setValue(target.value)
+    onChange?.(value)
   }
 
   return (
-    <div>
+    <div className={className}>
       <label
         htmlFor={name}
         className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -24,6 +39,7 @@ export default function Input(props: Readonly<IInputProps>) {
       </label>
 
       <input
+        value={value}
         id={name}
         name={name}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
--- ui/src/components/Input/types/index.ts
+++ ui/src/components/Input/types/index.ts
@@ -1,5 +1,8 @@
+import { Dispatch, SetStateAction } from "react"
+
 export interface IInputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
   name: string
   label?: string
-  onChange?: (value: string) => void
+  value?: string | number,
+  onChange?: Dispatch<any>
 }
